18d26b993b1c73d49365785b1709937b476bb98d
User54681 <anzolotylh@gmail.com>
2024-02-12 19:03:10 +0300

    Добавьте файлы проекта.

---
+++ vending_machine.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+class cash_register {
+private:
+    int cash_on_hand;
+public:
+    ~cash_register() {
+
+    }
+    cash_register(int cash_in) {
+
+    }
+    int get_current_balance() {
+
+    }
+    void accept_amount() {
+
+    }
+};
+
+class dispenser_type {
+private:
+    int number_of_items;
+    int type_cost;
+public:
+    ~dispenser_type() {
+
+    }
+    dispenser_type(int number_of_items, int type_cost) {
+
+    }
+    int get_no_of_items() {
+
+    }
+    int get_cost() {
+
+    }
+    void make_sale() {
+
+    }
+};
+
+int main()
+{
+    
+}
